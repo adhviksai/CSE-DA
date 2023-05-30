be22b72ad95ba59f8916bf218deed1a9b9bc9de5
Adhvik Sai <135026457+adhviksai@users.noreply.github.com>
2023-05-30 22:12:38 +0530

    Update Undergroundpath.cpp

--- Undergroundpath.cpp
+++ Undergroundpath.cpp
@@ -1,10 +1,10 @@
 #include<iostream>
-class GateController {
-private:
+class GateController {  //Creating the main class
+private: //private members
     bool opengate;
     double waterLevelThreshold;
 
-public:
+public: //public members
     GateController(double threshold) : opengate(true), waterLevelThreshold(threshold) {
         
         void checkWaterLevel(double waterLevel) {
@@ -15,19 +15,19 @@ public:
         }
     }
 
-private:
-    void closeGates() {
+private: //private members
+    void closeGates() {  //function for closing the gates
         opengate = false;
         cout<< "Closing the gates of the underground walkway."<<endl;
     }
 
-    void openGates() {
+    void openGates() {  //function for opening the gates
         opengate = true;
         cout<<"Opening the gates of the underground walkway."<<endl;
     }
 };
 
-int main() {
+int main() {  //main function
     double waterLevel;
     double threshold;
 
